dee862c256fab405bfe132b7dfd6acb5c8fd9bae
Samuel Eko <Master-Longhord>
2025-10-08 15:45:55 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#043873',
+        'secondary': '#4F9CF9',
+        'accent-yellow': '#FFE492',
+        'accent-blue': '#A7CEFC',
+        'dark-gray': '#212529',
+        'white': '#FFFFFF',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+    screens: {
+      'sm': '320px',
+      'md': '768px',
+      'lg': '1440px',
+      'xl': '1552px',
+      '2xl': '1920px',
+    },
+  },
+  plugins: [],
+}
